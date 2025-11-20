5378276095ce503d5eedc824d1536bc470e46089
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2025-11-20 12:15:33 +0100

    reset to clean next16 app router setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-import TestFile from "./components/TestFile"
-import Header from "./components/Header";
-
 export default function Home() {
   return (
-    <main>
-      <Header />
-        <section className="flex flex-1 items-center justify-center">
-          <div className="grid grid-cols-2 gap-8 mt-12">
-            <Link className="flex items-center justify-center w-70 h-70 border border-[#a50000] rounded-2xl text-center text-lg font-medium hover:bg-[#a50000] hover:border-0 hover:text-white transition" href={'fiches-pathologies'}>fiches pathologies</Link>
-            <Link className="flex items-center justify-center w-70 h-70 border border-[#a50000] rounded-2xl text-center text-lg font-medium hover:bg-[#a50000] hover:border-0 hover:text-white transition" href={'/orientation-diagnostique'}>orientation diagnostique</Link>
-            <Link className="flex items-center justify-center w-70 h-70 border border-[#a50000] rounded-2xl text-center text-lg font-medium hover:bg-[#a50000] hover:border-0 hover:text-white transition" href={'/situations-graves'}>situations graves</Link>
-            <Link className="flex items-center justify-center w-70 h-70 border border-[#a50000] rounded-2xl text-center text-lg font-medium hover:bg-[#a50000] hover:border-0 hover:text-white transition" href={'/situations-frequentes'}>situations fréquentes</Link>
-          </div>
-        </section>
-    </main>
+    <h1 style={{ padding: 50, fontSize: 40 }}>
+      🚀 HOME PAGE WORKS !
+    </h1>
   );
 }
